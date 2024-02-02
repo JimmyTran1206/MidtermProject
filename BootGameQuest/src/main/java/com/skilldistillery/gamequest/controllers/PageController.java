b61bbfd7e8c8f0c1ffd53cf63db18b9f6025438b
JonathonMcNamara <jonathonmcnamara1234@hotmail.com>
2024-02-02 14:37:16 -0700

    JSPs, PageController

---
+++ BootGameQuest/src/main/java/com/skilldistillery/gamequest/controllers/PageController.java
@@ -0,0 +1,33 @@
+package com.skilldistillery.gamequest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.skilldistillery.gamequest.data.UserDAO;
+import com.skilldistillery.gamequest.entities.User;
+
+@Controller
+public class PageController {
+	
+	@Autowired
+	private UserDAO userDAO;	
+	
+	@GetMapping("/GameDetails")
+	public String GameDetails() {
+		return "GameDetails";
+	}
+	
+	@GetMapping("CharacterDetails")
+	public String CharacterDeails() {
+		return "CharacterDetails";
+	}
+	
+	@GetMapping("GameList")
+	public String GameList() {
+		return "GameList";
+	}
+	
+
+}
